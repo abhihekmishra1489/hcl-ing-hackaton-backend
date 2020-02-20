1e07e64893269c65d7b2f5d423e6f7648f3dd44d
Abhishek Mishra 😈 <abhishekmishra@Abhisheks-MacBook-Pro.local>
2020-02-20 14:53:42 +0100

    adding aop and micrometer for prometheus dashboard

--- src/main/java/com/hcl/ing/adodenhaag/repository/ProductGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.ing.adodenhaag.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.ing.adodenhaag.entity.ProductDetails;
-
-public interface ProductGroupRepository extends JpaRepository<ProductDetails, Long> {
-
-}
