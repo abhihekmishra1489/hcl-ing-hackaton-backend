6024d4fc23473ddfe54e40bdbcda21e14a0088fc
ricardom57 <ricardom57@hotmail.com>
2020-02-20 18:49:03 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,5 +4,5 @@ echo 'The following Maven command installs your Maven-built Java application'
 set -x
 mvn package
 java -jar target/adodenhaag-backend-0.0.1-SNAPSHOT.jar > outputbe.txt &
-echo 'done
+echo 'done'
 exit 0
